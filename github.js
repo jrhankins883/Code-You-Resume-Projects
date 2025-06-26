76aecc2ebc10a18aaa669900512bfa2afcf535e6
John Hankins <jrhankins883@yahoo.com>
2025-06-26 17:21:29 -0400

    Added a media query to make it responsive on a smaller screen

--- github.js
+++ github.js
@@ -1,15 +1,17 @@
 const username = "jrhankins883";
 const url = `https://api.github.com/users/${username}/repos`;
 const list = document.getElementById("repo-list");
+const colors = ["#red", "blue", "green", "yellow", "orange", "pink", "purple"];
 
 fetch(url) 
     .then(response => response.json())
     .then(repos => {
         repos
             .filter(repo => repo.description)
-            .forEach(repo => {
+            .forEach((repo, index) => {
                 const li = document.createElement("li");
                 li.classList.add("repo-item");
+                li.style.borderColor = colors[index % colors.length];
                 li.innerHTML = `
                     <a href="${repo.html_url}" target="_blank" rel="noopener noreferrer">
                     ${repo.name}</a> - ${repo.description}
